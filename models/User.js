16ed9f88bbdaa5c9315c18bc9df68b8257eb4dc9
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-08-27 14:23:05 -0500

    updated user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use email address format."],
+    },
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+    ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
